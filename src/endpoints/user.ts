b39eb4e999c4f7f791b00ad45276deebd3ca2c13
Hammad Hassan <hammadhassan@Hammads-MacBook-Pro.local>
2023-01-10 23:44:07 +0500

    hh - Resolved conflicts

--- src/endpoints/user.ts
+++ src/endpoints/user.ts
@@ -1,8 +1,12 @@
 import SpotifiedSubClient from '../client/client.subclient';
-import { UserProfile } from '../types';
+import { CurrentUserProfile, UserProfile } from '../types/user.types';
 
 export default class User extends SpotifiedSubClient {
   getCurrentUserProfile() {
-    return this.get<UserProfile>('/me');
+    return this.get<CurrentUserProfile>('/me');
+  }
+
+  getUserProfile(userId: string) {
+    return this.get<UserProfile>(`/users/${userId}`);
   }
 }
