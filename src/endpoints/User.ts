7ed1af6dd81ca972e061dad6994b4e6e52380b7f
Hammad Hassan <hammadhassan@folio3.com>
2023-04-06 22:22:13 +0500

    :sparkles: Add User/Artist endpoints

---
+++ src/endpoints/User.ts
@@ -0,0 +1,18 @@
+import { ReadWriteBaseClient } from '../client/ReadWriteBaseClient';
+import { CurrentUserProfile, UserProfile } from '../types';
+
+export class User extends ReadWriteBaseClient {
+  getCurrentUserProfile() {
+    return this.get<CurrentUserProfile>('/me');
+  }
+
+  getUserProfile(userId: string) {
+    return this.get<UserProfile>(`/users/${userId}`);
+  }
+
+  followPlaylist(playlistId: string, options?: { public: boolean }) {
+    return this.put(`/playlists/${playlistId}/followers`, options);
+  }
+}
+
+export default User;
