91cf209560c7bd75f3556a4f744806968cc8c149
Abdullah Malik <abdullahmalik61@gmail.com>
2024-07-01 13:34:42 -0400

    Fix git guardian issue

--- src/__tests__/client/Spotified.test.ts
+++ src/__tests__/client/Spotified.test.ts
@@ -13,7 +13,7 @@ describe('Spotified', () => {
   const mockClientSecret = 'mock-client-secret';
   const mockAuthHeaders = {
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': `sefwfvcad_fwe_e3r123`,
+    'Authorization': ``,
   };
 
   beforeEach(() => {
