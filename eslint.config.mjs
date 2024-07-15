529f366a850c670e420b55f95d27d377ad7601e3
Abdullah Malik <abdullahmalik61@gmail.com>
2024-07-15 13:44:22 -0400

    changes for linter workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,71 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
+export default tseslint.config(
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
-];
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.ts', '.js', '.json'],
+        },
+      },
+      react: {
+        pragma: 'React',
+        version: 'detect',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      react: eslintPluginReact,
+      'react-hooks': eslintPluginReactHooks,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'max-len': ['warn', { code: 120, tabWidth: 2 }],
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+      'prefer-destructuring': ['warn', { object: true, array: false }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-underscore-dangle': 'off',
+      ...eslintPluginReact.configs.recommended.rules,
+      ...eslintPluginReactHooks.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+    },
+  },
+  {
+    files: ['**/*.test.ts'],
+    rules: {
+      'dot-notation': 'off',
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  }
+);
