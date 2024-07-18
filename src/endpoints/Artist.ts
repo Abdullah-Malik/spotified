cd471e40771ebd2fd84492a4e908351f60597b1f
Abhinav Sachdeva <avs7793@psu.edu>
2024-07-18 16:14:18 -0400

    added 3 endpoints with 1 issue

--- src/endpoints/Artist.ts
+++ src/endpoints/Artist.ts
@@ -1,5 +1,11 @@
 import { ReadWriteBaseClient } from '../client/ReadWriteBaseClient';
-import { Artist as ArtistProfile, ArtistsAlbumPageResult } from '../types';
+import { 
+  ArtistProfile,
+  ArtistsAlbumPageResult,
+  Artists
+  Tracks as TracksDetail,
+} from '../types';
+import { joinIdsArrayToString } from '../utils';
 import { SpotifiedPaginator } from '../paginators';
 import { SimplifiedAlbum } from '../types/album.types';
 
@@ -13,6 +19,14 @@ export class Artist extends ReadWriteBaseClient {
   }
 
   /**
+   * Get Spotify catalog information for several artists based on their Spotify IDs
+   * https://developer.spotify.com/documentation/web-api/reference/get-multiple-artists
+   */
+  getArtists(ids: string[]) {
+    return this.get<Artists>(`/artists?ids=${joinIdsArrayToString(ids)}`);
+  }
+
+  /**
    * Get Spotify catalog information about an artist's albums
    * https://developer.spotify.com/documentation/web-api/reference/get-an-artists-albums
    */
@@ -22,6 +36,22 @@ export class Artist extends ReadWriteBaseClient {
       requestMaker: this._requestMaker,
       queryParams: {},
     });
+  }
+
+  /**
+   * Get Spotify catalog information about an artist's top tracks by country
+   * https://developer.spotify.com/documentation/web-api/reference/get-an-artists-top-tracks
+   */
+  getArtistTopTracks(id: string, market?: string) {
+      return this.get<TracksDetail>(`/artist/${id}/top-tracks`);//unsure how to join market with this ??
+  }
+
+  /**
+   * Get Spotify catalog information about artists similar to a given artist.
+   * https://developer.spotify.com/documentation/web-api/reference/get-an-artists-related-artists
+   */
+  getRelatedArtists(id: string) {
+    return this.get<Artists>(`/artists/${id}/related-artists`)
   }
 }
 
