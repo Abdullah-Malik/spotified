7cc8e599582d13f94ee3c50ec0e7de51463d1e30
Abhinav Sachdeva <avs7793@psu.edu>
2024-07-18 15:50:16 -0400

    some documentation

--- src/endpoints/Artist.ts
+++ src/endpoints/Artist.ts
@@ -4,10 +4,18 @@ import { SpotifiedPaginator } from '../paginators';
 import { SimplifiedAlbum } from '../types/album.types';
 
 export class Artist extends ReadWriteBaseClient {
+    /**
+   * Get Spotify catalog information for a single artist identified by their unique Spotify ID
+   * https://developer.spotify.com/documentation/web-api/reference/get-an-artist
+   */
   getArtist(id: string) {
     return this.get<ArtistProfile>(`/artists/${id}`);
   }
 
+  /**
+   * Get Spotify catalog information about an artist's albums
+   * https://developer.spotify.com/documentation/web-api/reference/get-an-artists-albums
+   */
   getArtistAlbums(artistId: string) {
     return new SpotifiedPaginator<SimplifiedAlbum, ArtistsAlbumPageResult, { hello: string }>({
       endpoint: `https://api.spotify.com/v1/artists/${artistId}/albums`,
