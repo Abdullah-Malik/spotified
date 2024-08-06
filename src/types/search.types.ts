e94c11723d72149daf8d449301ec84655f7125fa
Abhinav Sachdeva <avs7793@psu.edu>
2024-08-06 13:19:07 -0400

    saving it

--- src/types/search.types.ts
+++ src/types/search.types.ts
@@ -1,6 +1,12 @@
+import { Track } from "endpoints";
 import { PaginationParams } from "./shared.types";
 
 export interface searchOptionalParams extends PaginationParams{
     market?: string;
     includeExternal?: string;
+}
+
+export interface searchResponse {
+    track?: Track;
+    
 }
