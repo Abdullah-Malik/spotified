f9a5bd54e42c5daf4c60a184285d774752310ef9
Abhinav Sachdeva <abhinav.sachdeva9@gmail.com>
2024-08-15 23:26:46 -0400

    Add user endpoints
    * added 1 endpoint
    
    * ADDING ENDPOINTS TO USER
    
    * some more additions & simplifications, will fail tests
    
    * added 1 endpoint
    
    * ADDING ENDPOINTS TO USER
    
    * some more additions & simplifications, will fail tests
    
    * rebased the branch
    
    * adding 3 more endpoints
    
    * small modifications
    
    * adding 1 more endpoint to user
    
    * fix for file path
    
    * update user methods and types
    
    * comments
    
    * more comments
    
    * completing comments
    
    * minor user endpoint fix
    
    ---------
    
    Co-authored-by: Abdullah Malik <abdullahmalik61@gmail.com>

--- src/endpoints/Genre.ts
+++ src/endpoints/Genre.ts
@@ -1,6 +1,10 @@
 import { ReadWriteBaseClient } from '../client/ReadWriteBaseClient';
 
 export class Genre extends ReadWriteBaseClient {
+  /**
+   * Retrieve a list of available genres seed parameter values for recommendations.
+   * https://developer.spotify.com/documentation/web-api/reference/get-recommendation-genres
+   */
   getAvailableGenreSeeds() {
     return this.get<{ genres: string[] }>('/recommendations/available-genre-seeds');
   }
