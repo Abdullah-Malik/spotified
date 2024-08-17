cedd9c4e9b5ab8557c35064c696542c9b7a7db38
Abdullah Malik <abdullahmalik61@gmail.com>
2024-08-16 22:59:14 -0400

    fix search endpoint and types

--- src/endpoints/Search.ts
+++ src/endpoints/Search.ts
@@ -1,13 +1,20 @@
 import { ReadWriteBaseClient } from '../client/ReadWriteBaseClient';
-import { searchOptionalParams, searchResponse } from '../types';
+import { SearchOptionalParams, SearchResponse } from '../types';
 import joinIdsArrayToString from '../utils';
 
 export class Search extends ReadWriteBaseClient {
-    /**
-     * Get Spotify catalog information about albums, artists, playlists, tracks, shows, episodes or audiobooks that match a keyword string. Audiobooks are only available within the US, UK, Canada, Ireland, New Zealand and Australia markets.
-     * https://developer.spotify.com/documentation/web-api/reference/search
-     */
-    searchForItem(q: string, type: string[], optionalParams?: searchOptionalParams){
-        return this.get<searchResponse>(`/search`,{ q, type: joinIdsArrayToString(type), ...optionalParams });
-    }
+  /**
+   * Get Spotify catalog information about albums, artists, playlists, tracks, shows, episodes or
+   * audiobooks that match a keyword string.
+   * https://developer.spotify.com/documentation/web-api/reference/search
+   */
+  searchForItem(q: string, type: string[], optionalParams?: SearchOptionalParams) {
+    return this.get<SearchResponse>(`/search`, {
+      q: encodeURIComponent(q),
+      type: joinIdsArrayToString(type),
+      ...optionalParams,
+    });
+  }
 }
+
+export default Search;
--- src/types/search.types.ts
+++ src/types/search.types.ts
@@ -1,24 +1,24 @@
-import { Tracks } from './track.types';
+import { Track } from './track.types';
 import { PaginationParams } from './shared.types';
 import { PaginationResponseProps } from './paginator.types';
 import { SimplifiedAlbum } from './album.types';
-import { Artists } from './artist.types';
+import { Artist } from './artist.types';
 import { SimplifiedPlaylist } from './playlist.types';
 import { SimplifiedShow } from './show.types';
 import { SimplifiedEpisode } from './episode.types';
 import { SimplifiedAudiobook } from './audiobook.types';
 
-export interface searchOptionalParams extends PaginationParams{
-    market?: string;
-    includeExternal?: string;
+export interface SearchOptionalParams extends PaginationParams {
+  market?: string;
+  includeExternal?: string;
 }
 
-export interface searchResponse {
-    tracks?: (Tracks & PaginationResponseProps);
-    artists?: (Artists & PaginationResponseProps);
-    albums?: (SimplifiedAlbum[] & PaginationResponseProps);
-    playlists?: (SimplifiedPlaylist[] & PaginationResponseProps);
-    shows?: (SimplifiedShow[] & PaginationResponseProps);
-    episodes?: (SimplifiedEpisode[] & PaginationResponseProps);
-    audiobooks?: (SimplifiedAudiobook[] & PaginationResponseProps);
+export interface SearchResponse {
+  tracks?: { items: Track[] } & PaginationResponseProps;
+  artists?: { items: Artist[] } & PaginationResponseProps;
+  albums?: { items: SimplifiedAlbum[] } & PaginationResponseProps;
+  playlists?: { items: SimplifiedPlaylist[] } & PaginationResponseProps;
+  shows?: { items: SimplifiedShow[] } & PaginationResponseProps;
+  episodes?: { items: SimplifiedEpisode[] } & PaginationResponseProps;
+  audiobooks?: { items: SimplifiedAudiobook[] } & PaginationResponseProps;
 }
