7c5d509b87e626e123e29e55c0b027016cd499af
Abdullah Malik <abdullahmalik61@gmail.com>
2024-08-23 20:34:32 -0400

    refactor makeRequest method

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const AUTHORIZE_URL = 'https://accounts.spotify.com/authorize';
 export const API_TOKEN_URL = 'https://accounts.spotify.com/api/token';
+export const SPOTIFY_API_URL = 'https://api.spotify.com/v1';
